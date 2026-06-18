d626a2d2d25cf0861c396415fcf32f5745998fdc
kusumisui-ship-it <kusumisui@gmail.com>
2026-06-18 18:33:02 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let money = 1000;
 
+const moneyText = document.getElementById("money");
+
+const work = document.getElementById("work");
+
+
+work.onclick = ()=>{
+
+money += 100;
+
+
+moneyText.textContent = `資金：${money}円`;
+
+}
